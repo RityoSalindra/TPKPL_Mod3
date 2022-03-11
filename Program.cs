894a4f6c351484642bbb4c9af19bad4134c8a98d
Rityo Salindra <nuruddin859@gmail.com>
2022-03-11 16:49:46 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -11,11 +11,45 @@ namespace tpmodul3_1302204014
             KodePos kode = new KodePos();
 
             Console.WriteLine();
-            kode.getKodePos();
+            //kode.getKodePos();
+            DoorMachine pintu = new DoorMachine();
+            pintu.kunci();
 
         }
 
+        class DoorMachine
+        {
+            enum State { TERKUNCI, TERBUKA };
+            public void kunci()
+            {
+                State state = State.TERKUNCI;
+
+                String[] screenName = { "TERKUNCI", "TERBUKA" };
+                do
+                {
+                    Console.WriteLine("PINTU " + screenName[(int)state]);
+                    Console.Write("Enter Command : ");
+                    String command = Console.ReadLine();
+                    switch (state)
+                    {
+                        case State.TERKUNCI:
+                            if (command == "BukaPintu")
+                            {
+                                state = State.TERBUKA;
+                            }
+                            break;
+                        case State.TERBUKA:
+                            if (command == "KunciPintu")
+                            {
+                                state = State.TERKUNCI;
+                            }
+                            break;
+
+
+
+                    }
+                } while (state != State.TERKUNCI);
+            }
+        }
     }
-
 }
-
